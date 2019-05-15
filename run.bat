4bd1f564bcff9bc4bfe46ac9de25094cbee6b7b6
BK KIM <cdeclare@gmail.com>
2019-05-16 01:25:48 +0900

    Add File Delete

--- run.bat
+++ run.bat
@@ -1,8 +1,7 @@
 
+rm -f *.txt
+
 c++\main
-
 dotnet dotnetcore\bin\Release\netcoreapp2.2\netcon.dll
-
 java -cp java Main
-
 python python\main.py
--- run.sh
+++ run.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
+rm -f *.txt
+
 c++/main
-
 dotnet dotnetcore/bin/Release/netcoreapp2.2/netcon.dll
-
 java -cp java Main
-
 python python/main.py
