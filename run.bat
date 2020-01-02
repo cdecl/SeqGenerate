9bb8300210c1fe94f2c4cd405b4a8ed9315d9b11
N15479 <cdeclare@interpark.com>
2020-01-02 15:40:44 +0900

    Update run script

--- run.bat
+++ run.bat
@@ -1,9 +1,20 @@
 
 rm -f *.txt
 
+echo off 
+
+sleep 1
 c++\main
+
+sleep 1
 dotnet dotnetcore\bin\Release\netcoreapp3.1\netcon.dll
+
+sleep 1
 golang\bin\main.exe
+
+sleep 1
 java -cp java Main
 
 REM python python\main.py
+
+wc -l *.txt
--- run.sh
+++ run.sh
@@ -9,9 +9,11 @@ sleep 1
 dotnet dotnetcore/bin/Release/netcoreapp3.1/netcon.dll
 
 sleep 1
-golang/bin/main.exe 
+golang/bin/main.exe
 
 sleep 1
 java -cp java Main
 
 # python python/main.py
+
+wc -l *.txt
