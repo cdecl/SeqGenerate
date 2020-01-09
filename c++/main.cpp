507ec0d5b609ba82f85ac67cc3efe2fdeb26a561
BK KIM <cdeclare@gmail.com>
2020-01-10 03:19:24 +0900

    Update stringstream to fmt::memory_buffer

--- c++/main.cpp
+++ c++/main.cpp
@@ -8,19 +8,18 @@
 #include <iterator>
 using namespace std;
 
-const char code[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-// const char code[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G'};
+#include "fmt/format.h"
 
+const char code[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 
 int main(int argc, char const *argv[])
 {
 	clock_t start = clock();
-
 	{
 		char sz[128];
-		stringstream oss;
+		// stringstream oss;
 		int n = 0;
-
+		fmt::memory_buffer buff;
 		// string s;ake
 		ofstream fout("seq-c++.txt");
 		for (auto &c1 : code) {
@@ -35,11 +34,16 @@ int main(int argc, char const *argv[])
 							sz[4] = c5;
 							sz[5] = '\n';
 							sz[6] = '\0';
-							oss.write(sz, 6);
+
+							// oss.write(sz, 6);
+							buff.append(sz, sz + 6);
 
 							if (!(n % 4096)) {
-								fout << oss.rdbuf();
-								oss.str("");
+								// fout << oss.rdbuf();
+								// oss.str("");
+
+								fout.write(buff.data(), buff.size());
+								buff.clear();
 								n = 0;
 							}
 							++n;
@@ -48,13 +52,15 @@ int main(int argc, char const *argv[])
 				}
 			}
 		}
-		fout << oss.rdbuf();
+		// fout << oss.rdbuf();
+		fout.write(buff.data(), buff.size());
+		buff.clear();
 	}
 
 	clock_t end = clock();
 	// long duration = (end - start) ;
 	double duration = (double)(end - start) / CLOCKS_PER_SEC;
 	cout << "c++ : " << duration << " sec" << endl;
-			
+
 	return 0;
 }
